a57177907095840b38cfcd467e5d3222c1a33a39
hawaicincoceronomams@gmail.com <hawaicincoceronomams@gmail.com>
2022-03-10 11:11:31 -0600

    Agregar archivos de proyecto.

---
+++ Conexión_MySql/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Conexión_MySql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_host = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_db = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_psw = new System.Windows.Forms.TextBox();
+            this.btn_conectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Conexión a MySql Workbench";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(117, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Host";
+            // 
+            // txt_host
+            // 
+            this.txt_host.Location = new System.Drawing.Point(170, 135);
+            this.txt_host.Name = "txt_host";
+            this.txt_host.Size = new System.Drawing.Size(164, 22);
+            this.txt_host.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(348, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Base de Datos";
+            // 
+            // txt_db
+            // 
+            this.txt_db.Location = new System.Drawing.Point(487, 138);
+            this.txt_db.Name = "txt_db";
+            this.txt_db.Size = new System.Drawing.Size(120, 22);
+            this.txt_db.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(94, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Usuario";
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(170, 192);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(164, 22);
+            this.txt_user.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(372, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Contraseña";
+            // 
+            // txt_psw
+            // 
+            this.txt_psw.Location = new System.Drawing.Point(487, 191);
+            this.txt_psw.Name = "txt_psw";
+            this.txt_psw.Size = new System.Drawing.Size(120, 22);
+            this.txt_psw.TabIndex = 8;
+            this.txt_psw.UseSystemPasswordChar = true;
+            // 
+            // btn_conectar
+            // 
+            this.btn_conectar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn_conectar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_conectar.Location = new System.Drawing.Point(487, 264);
+            this.btn_conectar.Name = "btn_conectar";
+            this.btn_conectar.Size = new System.Drawing.Size(120, 38);
+            this.btn_conectar.TabIndex = 9;
+            this.btn_conectar.Text = "Conectar";
+            this.btn_conectar.UseVisualStyleBackColor = false;
+            this.btn_conectar.Click += new System.EventHandler(this.btn_conectar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(746, 450);
+            this.Controls.Add(this.btn_conectar);
+            this.Controls.Add(this.txt_psw);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_db);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_host);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_host;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_db;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_psw;
+        private System.Windows.Forms.Button btn_conectar;
+    }
+}
+
---
+++ Conexión_MySql/Form1.cs
@@ -0,0 +1,40 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conexión_MySql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_conectar_Click(object sender, EventArgs e)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString = "server="+txt_host.Text+";uid="+txt_user.Text+";pwd="
+                +txt_psw.Text+";database="+txt_db.Text;
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                MessageBox.Show("Conexión realizada con éxito!");
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
